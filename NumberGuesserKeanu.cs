d0aa8d1a2bd95fadd0fb68430b4a7a8477813a98
Kurizaki <110892283+Kurizaki@users.noreply.github.com>
2022-09-08 12:48:14 +0200

    Add files via upload

---
+++ NumberGuesserKeanu.cs
@@ -0,0 +1,25 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.3.32811.315
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "NumberGuessingAdvancedDegin", "..\source\repos\NumberGuessingAdvancedDegin\NumberGuessingAdvancedDegin\NumberGuessingAdvancedDegin.csproj", "{74A8389E-1F54-4B56-9B61-74CEE67B7685}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{74A8389E-1F54-4B56-9B61-74CEE67B7685}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{74A8389E-1F54-4B56-9B61-74CEE67B7685}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{74A8389E-1F54-4B56-9B61-74CEE67B7685}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{74A8389E-1F54-4B56-9B61-74CEE67B7685}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {40C4FD36-5713-4A39-9D00-72A32A374BDF}
+	EndGlobalSection
+EndGlobal
